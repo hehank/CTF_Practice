a74a35c6492bbdea7899dc6e5bd840d2c7c397f8
hehank <macb3634919@gmail.com>
2022-05-27 14:26:56 +0800

    fix

---
+++ Pwn/CTF/Angelboy_Pwn-2/ret2sc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char name[50];
+
+int main()
+{
+    setvbuf(stdout, 0, 2, 0);
+    printf("Name:");
+
+    read(0, name, 50);
+
+    char buf[20];
+
+    printf("Try your best:");
+
+    gets(buf);
+    return 0;
+}
